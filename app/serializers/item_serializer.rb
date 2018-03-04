28a17d9fab05c06a436ed6eddf690969c00df694
mimasofia <virginiadonaire@hotmail.com>
2018-03-04 17:21:04 -0500

    Adds has many and belongs to
    In the serializer for items adds belongs to and in places adds has
    many items

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,3 +1,4 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :item_name
+  belongs_to :place
 end
--- app/serializers/place_serializer.rb
+++ app/serializers/place_serializer.rb
@@ -1,3 +1,4 @@
 class PlaceSerializer < ActiveModel::Serializer
   attributes :id, :name
+  has_many :items
 end
