4fd77b1dd295c9835c7d475b6f9468cdbdd04c42
mimasofia <virginiadonaire@hotmail.com>
2018-03-04 16:02:16 -0500

    Runs scaffold for items
    Ran `generate scaffold` for items to stand alone for now. Ran
    `db:migrate` and made a curl script to create and tested it.
    
    Need to add a reference to `places` still

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180304203637) do
+ActiveRecord::Schema.define(version: 20180304205702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20180304203637) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_examples_on_user_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "item_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "places", force: :cascade do |t|
---
+++ scripts/items/create.sh
@@ -0,0 +1,10 @@
+curl "http://localhost:4741/items" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{
+    "item": {
+      "item_name": "'"${ITEM}"'"
+    }
+  }'
