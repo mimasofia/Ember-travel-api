ac238e9c7264ac59294db4b977ea252babe99030
mimasofia <virginiadonaire@hotmail.com>
2018-03-07 10:30:43 -0500

    Adds def for place in items
    Added a def for place with the id in the items serializer. This
    seemed to fix previous issue about API JSON format in front end

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -2,7 +2,7 @@ class ItemSerializer < ActiveModel::Serializer
   attributes :id, :item_name, :place
   belongs_to :place
 
-  # def list
-  #   object.place.id
-  # end
+  def place
+    object.place.id
+  end
 end
